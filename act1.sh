31304378e1c5820639faf72b2ee1e443d8567d4d
lorenzalejandrino <158862246+lorenzalejandrino@users.noreply.github.com>
2025-02-05 09:43:26 +0800

    Add files via upload

---
+++ act1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "enter your name:"
+read NAME
+
+echo "enter your age:"
+read AGE
+
+echo "enter your program"
+read PROGRAM
+
+echo "Hello, $NAME! your age is $AGE, & your program is $PROGRAM"
+
+
+
