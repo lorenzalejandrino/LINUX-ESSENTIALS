f985c2521631560ecc7cec0132a85c2209296070
lorenzalejandrino <158862246+lorenzalejandrino@users.noreply.github.com>
2025-04-02 12:02:05 +0800

    Update act6.sh

--- act6.sh
+++ act6.sh
@@ -1,13 +1,14 @@
+LOG_FILE="log.txt"
 GITHUB_USERNAME="${GITHUB_USER:-unknown_user}"
 
-if [ -f "log2.txt" ]; then
-    COUNT=$(wc -l < "log2.txt")
+
+if [ -f "$LOG_FILE" ]; then
+    COUNT=$(wc -l < "$LOG_FILE")
 else
     COUNT=0
 fi
 
-# Increment the count
+
 COUNT=$((COUNT + 1))
 
-# Log the update with count, timestamp, and username
-echo "Update #$COUNT - Current Date and Time: $(date) - Updated by $GITHUB_USERNAME" >> "log2.txt"
+echo "Update #$COUNT - Current Date and Time: $(date) - Updated by $GITHUB_USERNAME" >> "$LOG_FILE"
